b0c9dde98a4d044b8dc610f67f64dd06c70b6a46
Tomas Pustelnik <10606260+Pustelto@users.noreply.github.com>
2020-07-17 12:19:01 +0200

    Update setup.sh

--- setup_scripts/setup.sh
+++ setup_scripts/setup.sh
@@ -1,6 +1,22 @@
-# set git, npm, brew
-# set mac settings (hidden folders tabing in dialogs etc.)
-# download dotfiles
-# restore brew and npm
+# install xcode tools
+
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew bundle install --file=~/Dotfiles/backup/Brewfile
+
+# install node and global node packages
+# We need to isntall node from the web so we have to do it manually
+~/Dotfiles/setup_scripts/npm_install.sh
+
 # restore zsh/dotfiles
+sudo pip3 install dotfiles
+
+dotfiles ---sync
+
 # restore vs code settings/plugins
+# Need to check if snippets folder exist, on fresh install it will not so we need to create it
+~/Dotfiles/setup_scripts/code_install.sh
+
+# set mac settings (hidden folders tabing in dialogs etc.)
+
