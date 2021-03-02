c4f96d73200dba33e291dbcd813f8bffb85b9a21
Tomas Pustelnik <10606260+Pustelto@users.noreply.github.com>
2021-03-02 16:57:57 +0100

    fix code setup when files already exists

--- setup_scripts/code_install.sh
+++ setup_scripts/code_install.sh
@@ -2,8 +2,16 @@
 
 DOTFILES_CODE_SETTINGS=~/Library/Application\ Support/Code/User
 
+# Install extentions
+if cat ~/Dotfiles/backup/code_extensions | xargs -L 1 code --install-extension
+  then
+    echo "VS Code extensions installed successfully, please reload your VS Code"
+  else
+    echo "VS Code extensions installation error"
+fi
+
 # Link VS Code settings
-if ln ~/Dotfiles/code/settings/* "$DOTFILES_CODE_SETTINGS"
+if ln -f ~/Dotfiles/code/settings/* "$DOTFILES_CODE_SETTINGS"
   then
     echo "VS Code settings linked successfully"
   else
@@ -16,12 +24,4 @@ if ln ~/Dotfiles/code/snippets/* "$DOTFILES_CODE_SETTINGS"/snippets
     echo "VS Code snippets linked successfully"
   else
     echo "VS Code snippets linking error"
-fi
-
-# Install extentions
-if cat ~/Dotfiles/backup/code_extensions | xargs -L 1 code --install-extension
-  then
-    echo "VS Code extensions installed successfully, please reload your VS Code"
-  else
-    echo "VS Code extensions installation error"
 fi
